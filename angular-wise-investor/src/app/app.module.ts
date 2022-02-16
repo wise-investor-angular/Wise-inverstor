60eb0ca0d827fc9a8d99540630e2107cc9d2d37e
sarra-benali <sarraba4@gmail.com>
2022-02-16 14:05:03 +0100

    mainfeed

--- angular-wise-investor/src/app/app.module.ts
+++ angular-wise-investor/src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InitiateYourProjectComponent } from './initiate-your-project/initiate-your-project.component';
-import {SecurityModule} from './security/security.module'
+
+import {SecurityModule} from './security/security.module';
+import { MainfeedComponent } from './mainfeed/mainfeed.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    InitiateYourProjectComponent
+    InitiateYourProjectComponent,
+    MainfeedComponent,
+  
   ],
   imports: [
     BrowserModule,
