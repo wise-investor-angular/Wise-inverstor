86436a4e187b0f318c43d3e11b271afa26cd965b
wassimghommidh <wassghom8@gmail.com>
2022-02-16 11:49:31 +0100

    update login&signupV1.3

--- angular-wise-investor/src/app/app.module.ts
+++ angular-wise-investor/src/app/app.module.ts
@@ -3,19 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InitiateYourProjectComponent } from './initiate-your-project/initiate-your-project.component';
-import {SecurityModule} from './security/security.module';
-import { NavbarComponent } from './navbar/navbar.component'
+import { SecurityModule } from './security/security.module';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    InitiateYourProjectComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    SecurityModule
-  ],
+  declarations: [AppComponent, InitiateYourProjectComponent],
+  imports: [BrowserModule, SecurityModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- angular-wise-investor/src/app/security/login/login.component.ts
+++ angular-wise-investor/src/app/security/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ServeService } from '../serve.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,8 +9,13 @@ import { NgForm } from '@angular/forms';
 export class LoginComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
   onSubmit(f: NgForm) {
+    const loginobserver = {
+      // next: (x: number) => console.log('Observer'),
+      // error: (err: Error) => console.error(err),
+    };
+    // this.securityservice.login(f.value).subscribe(loginobserver);
     console.log(f.value);
     console.log(f.valid);
   }
