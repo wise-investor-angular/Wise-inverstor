446b945730b30c28086e0f6a57a41e3ec87e27e0
badis-naceur <naceurbadis@gmail.com>
2022-02-16 11:48:11 +0100

    angular router created with seperated navbar component

--- angular-wise-investor/src/app/app.module.ts
+++ angular-wise-investor/src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InitiateYourProjectComponent } from './initiate-your-project/initiate-your-project.component';
-import { AdminComponent } from './admin/admin.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InitiateYourProjectComponent,
-    AdminComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
