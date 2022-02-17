9ffe42de1a9973aa1cea6ce29166a991303a8dc2
wassimghommidh <wassghom8@gmail.com>
2022-02-17 23:28:58 +0100

    email receiving feature

--- angular-wise-investor/src/app/app.module.ts
+++ angular-wise-investor/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { FormsModule } from '@angular/forms';
     AdminComponent,
     InitiateYourProjectComponent,
     AppComponent,
-FilterComponent,
+    FilterComponent,
     NavbarComponent,
     InitiateYourProjectComponent,
     ShareyourexperienceComponent,
@@ -28,10 +28,11 @@ FilterComponent,
     FeedComponent,
     ProjectComponent,
     FilterComponent,
+  
     // HttpClientModule,
-
+    
   ],
-  imports: [BrowserModule, AppRoutingModule, SecurityModule,FormsModule],
+  imports: [BrowserModule, AppRoutingModule, SecurityModule, FormsModule],
 
   providers: [],
   bootstrap: [AppComponent],
